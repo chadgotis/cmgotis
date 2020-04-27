c398ec1a74d827d4881fbfdef3f21565a48a8b8a
Chad Gotis <Chadgotis97@gmail.com>
2020-04-28 02:54:21 +0800

    Resposive design for tablet

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const menu = document.querySelector("#menu");
 const side = document.querySelector(".side-menu");
-console.log(side);
 
 menu.addEventListener("click", () => {
   side.classList.toggle("show-menu");
