2b1bce08e4b67a9f8264f9170ed582ba202ee57d
Chad Gotis <Chadgotis97@gmail.com>
2020-04-29 02:30:10 +0800

    Finished Responsive design + animation

--- js/main.js
+++ js/main.js
@@ -4,3 +4,14 @@ const side = document.querySelector(".side-menu");
 menu.addEventListener("click", () => {
   side.classList.toggle("show-menu");
 });
+
+// const cards = document.querySelectorAll(".card");
+// cards.forEach((card) => {
+//   card.addEventListener("click", () => {
+//     card.classList.toggle("rubberBand");
+//   });
+// });
+
+window.addEventListener("load", () => {
+  new WOW().init();
+});
